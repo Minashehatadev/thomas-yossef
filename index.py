7fbce7ea620ef62c1155c328ca40342a8ef50d78
Minashehatadev <uhhrhjjj@gmail.com>
2024-05-29 15:05:01 +0300

    Update index.html

--- index.py
+++ index.py
@@ -1,10 +1,3 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'مرحباً بك في موقعي الإلكتروني!'
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
+<?php
+ echo "mina"
+  ?>
